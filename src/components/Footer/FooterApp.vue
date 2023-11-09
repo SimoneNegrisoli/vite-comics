8ca86ed34e73fec746b8250b21cb009424d2f10c
SimoneNegrisoli <simo.negrisoli@gmail.com>
2023-11-09 19:14:16 +0100

    footer top

--- src/components/Footer/FooterApp.vue
+++ src/components/Footer/FooterApp.vue
@@ -1,12 +1,23 @@
 <template>
   <footer>
+    <!-- top -->
     <div class="top">
-      <div class="container">
+      <div class="container text-center">
         <div class="row">
-          <div class="col-12">ciao</div>
+          <div class="col-12 col-md-2" v-for="(el, index) in  footerOpt " :key="index">
+            <div>
+              <img :src="`/buy-comics-${el.nameImg}`" alt="">
+
+            </div>
+            <div>
+
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
+    <!-- center -->
     <div class="center">
       <div class="container">
         <div class="row">
@@ -14,6 +25,7 @@
         </div>
       </div>
     </div>
+    <!-- bottom -->
     <div class="bottom">
       <div class="container">
         <div class="row">
@@ -29,6 +41,33 @@
 <script>
 export default {
   name: "FooterApp",
+  data() {
+    return {
+      footerOpt: [
+        {
+          optTitle: 'digital comics',
+          nameImg: 'digital-comics.png'
+        },
+        {
+          optTitle: 'dc merchandise',
+          nameImg: 'merchandise.png'
+        },
+        {
+          optTitle: 'subscription',
+          nameImg: 'subscriptions.png'
+        },
+        {
+          optTitle: 'dcomic shop locator',
+          nameImg: 'shop-locator.png'
+        },
+        {
+          optTitle: 'dc power visa',
+          nameImg: 'dc-power-visa.svg'
+        },
+      ]
+    }
+  }
+
 };
 </script>
 
